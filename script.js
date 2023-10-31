10a8b76b346f904f6edc002d7236f2cdf6cb6bc5
Abhishek-308 <148197212+Abhishek-308@users.noreply.github.com>
2023-10-31 07:26:58 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log("Wlcome to myTicTacToe");
 let music = new Audio("win.wav");
 let audioTurn = new Audio("click.wav");
 let gameOver = new Audio("lost.mp3");
+let winAudio = new Audio("win.wav")
 let turn = "❌"
 let isGameOver = false
 //Function to change the turn
@@ -13,20 +14,25 @@ const changeTurn = () => {
 const checkWin = () => {
     let boxText = document.getElementsByClassName('boxText')
     let wins = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
+        [0, 1, 2,5,5,0],
+        [3, 4, 5,5,15,0],
+        [6, 7, 8,5,25,0],
+        [0, 3, 6,-5,15,90],
+        [1, 4, 7,5,15,90],
+        [2, 5, 8,15,15,90],
+        [0, 4, 8,5,15,45],
+        [2, 4, 6,5,15,135],
     ]
     wins.forEach(e => {
         if ((boxText[e[0]].innerText === boxText[e[1]].innerText) && (boxText[e[2]].innerText === boxText[e[1]].innerText) && (boxText[e[0]].innerText !== "")) {
             document.querySelector('.info').innerText = boxText[e[0]].innerText + "Won"
             isGameOver = true;
-            document.querySelector('.imgBox').getElementsByTagName('img')[0].style.width = "100px"
+            document.querySelector('.imgBox').getElementsByTagName('img')[0].style.width = "200px"
+            document.querySelector(".line").style.transform=`translate(${e[3]}vw,${e[4]}vw) rotate(${e[5]}deg)`
+            document.querySelector(".line").style.width="20vw"
+            winAudio.play()
+            
+
 
         }
     })
@@ -64,5 +70,6 @@ reset.addEventListener('click',()=>{
     isGameOver=false
     document.getElementsByClassName("info")[0].innerText="Turn for"+turn;
     document.querySelector('.imgBox').getElementsByTagName('img')[0].style.width = "0"
+    document.querySelector(".line").style.width="0"
 
 })
